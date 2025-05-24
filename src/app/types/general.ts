2ae7a1c4867ddf803c2ce376235d374a2fcc8dcc
Tadeo Daniel <hector_lectric@hotmail.com>
2025-05-24 13:37:04 -0600

    app: add schemas and types

---
+++ src/app/types/general.ts
@@ -0,0 +1,34 @@
+import { loginSchema } from "@/app/schemas/loginSchema";
+import { InputHTMLAttributes } from "react";
+import { Control, FieldError } from 'react-hook-form';
+import { InferType } from "yup";
+
+export type FormValues = InferType<typeof loginSchema>;
+export type Variant = "primary" | "secondary";
+export type Size = "sm" | "md" | "lg";
+
+export interface LoginFormProps {
+  onSubmit: (data: FormValues) => void;
+  isSubmitting: boolean;
+};
+
+export interface InputFieldProps extends InputHTMLAttributes<HTMLInputElement> {
+  label: string;
+};
+
+export interface ControlledInputProps {
+  name: keyof FormValues;
+  label: string;
+  type: "text" | "email" | "password";
+  placeholder?: string;
+  control: Control<FormValues>;
+  error?: FieldError;
+}
+
+export interface ButtonProps
+  extends Omit<React.ButtonHTMLAttributes<HTMLButtonElement>, "children"> {
+  label: string;
+  variant?: Variant;
+  size?: Size;
+}
+
