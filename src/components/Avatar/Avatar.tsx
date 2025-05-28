91d47a6ec0416908aca55383843cf1747d5849d9
Tadeo Daniel <49538038+TadeoDaniel19@users.noreply.github.com>
2025-05-27 20:36:24 -0600

    component: add new variant to button and add avatar (#1)

---
+++ src/components/Avatar/Avatar.tsx
@@ -0,0 +1,26 @@
+'use client';
+import Image from "next/image";
+import React from "react";
+import { IAvatarProps } from "@/types/general";
+
+export function Avatar({
+  src,
+  alt = "Avatar",
+  size = 32,
+  className = "",
+}: IAvatarProps) {
+  return (
+    <div
+      className={`relative rounded-full overflow-hidden ${className}`}
+      style={{ width: size, height: size }}
+    >
+      <Image
+        src={src}
+        alt={alt}
+        width={size}
+        height={size}
+        className="object-cover"
+      />
+    </div>
+  );
+}
